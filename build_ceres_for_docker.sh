bccd6ff191991655f354310bac5c216c8ab0652d
jwdinius <josephwdinius@gmail.com>
2019-12-01 17:11:21 -0800

    adding functional docker implementation and how-to

--- build_ceres_for_docker.sh
+++ build_ceres_for_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-mkdir -p ceres
-cd ceres
+export ROOT_PATH=$1
+mkdir -p $ROOT_PATH/ceres
+cd $ROOT_PATH/ceres
 wget http://ceres-solver.org/ceres-solver-1.14.0.tar.gz
 tar xvzf ceres-solver-1.14.0.tar.gz
 cd ceres-solver-1.14.0
@@ -11,5 +11,4 @@ mkdir build
 cd build
 cmake ..
 make -j8
-mv ceres/ceres-solver-1.14.0/build/lib/libceres* lib/utils/extend_utils/lib
-
+sudo make install
